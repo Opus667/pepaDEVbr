a98842905f631feee542372fb00182766f360aaf
Pepa <pepa@pepa.dev.br>
2025-12-05 20:57:24 -0300

    E lá vamos nós!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-// import node from "@astrojs/node";
-import netlify from "@astrojs/netlify";
-import clerk from "@clerk/astro";
 
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://pepa.dev.br",
-  integrations: [mdx(), sitemap(), clerk()],
-
-  adapter: netlify(),
+  integrations: [mdx(), sitemap()],
 });

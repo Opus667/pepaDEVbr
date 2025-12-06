bc46765af4acd240649cea16aa5ab95acd2510d8
Pepa <pepa@pepa.dev.br>
2025-12-06 00:15:52 -0300

    E lá vamos nós!!!

--- public/identity-redirect.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  // Se a URL tem invite_token na hash do root
-  if (window.location.hash.includes("invite_token")) {
-    const token = window.location.hash;
-    // Redireciona para /admin preservando a hash inteira
-    window.location.href = "/admin/" + token;
-  }
-})();

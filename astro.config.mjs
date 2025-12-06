9dde7d4833bcbd5b601aab9f39138c2ec25bc607
Pepa <pepa@pepa.dev.br>
2025-12-06 03:31:52 -0300

    E lá vamos nós!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,47 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+// Remark
+import remarkMdx from "remark-mdx";
+import remarkDirective from "remark-directive";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import remarkEmoji from "remark-emoji";
+import remarkGemoji from "remark-gemoji";
+import remarkTextr from "remark-textr";
+import remarkTypograf from "@mavrin/remark-typograf";
+
+// Rehype
+import rehypeRaw from "rehype-raw";
+import rehypeFormat from "rehype-format";
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://pepa.dev.br",
+
+  integrations: [
+    mdx({
+      remarkPlugins: [
+        remarkMdx,
+        remarkFrontmatter,
+        remarkDirective,
+        remarkGfm,
+        remarkMath,
+        remarkToc,
+        [remarkEmoji, { emoticon: true }],
+        remarkGemoji,
+        remarkTextr,
+        remarkTypograf,
+      ],
+      rehypePlugins: [
+        rehypeRaw, // Permite HTML dentro do MDX
+        rehypeFormat, // Ajusta indentação/saída
+      ],
+    }),
+    sitemap(),
+  ],
 });

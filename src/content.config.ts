18b878811b78f5ca00d5bb4406a4354cc3ce2c24
Pepa <pepa@pepa.dev.br>
2025-12-23 20:26:15 -0300

    0.0.3 - Mais um reset...

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,31 +16,21 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
-import { z, defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
-
-const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
-  schema: z.object({
-    title: z.string(),
-    permalink: z.string().optional(),
-  }),
-});
-
-// Expose your defined collection to Astro
-// with the `collections` export
-
 const projects = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
-  schema: z.object({
-    title: z.string(),
-    permalink: z.string().optional(),
-  }),
+  // Load Markdown and MDX files in the `src/content/projects/` directory.
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
 });
 
-// Expose your defined collection to Astro
-// with the `collections` export
 export const collections = {
   blog,
   projects,
